f58b75b0c4a7af0b43073c306396bbbd3f59eea2
Juan Quino <quinojuan@gmail.com>
2022-11-06 00:24:01 -0300

    layout completed

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -4,7 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons'
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -44,6 +44,14 @@ const Sidebar = () => {
             rel="noreferrer"
             href="https://www.linkedin.com/in/quinojuan/">
               <FontAwesomeIcon icon={faLinkedinIn} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a 
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.github/quinojuan/">
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
             </a>
           </li>
         </ul>
