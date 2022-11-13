64a0f8e118eac968634e74ece65a4e651d5d0b0a
Juan Quino <quinojuan@gmail.com>
2022-11-13 17:50:14 -0300

    comenté el useeffect del home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,35 @@
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
-  const jobArray = ["w", "e", "b", " ", "d", "e", "v", "e", "l", "o", "p", "e", "r", "."]
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+/* este useEffect lo tuve que sacar porque sino me compilaba pero no funcionaba*/
+  // useEffect(() => {
+  //   return setTimeout(() => {
+  //     setLetterClass('text-animate-hover')
+  //   }, 4000)
+  // }, [])
+  
   return (
     <div className="cantainer home-page">
       <div className="text-zone">
@@ -15,17 +37,21 @@ const Home = () => {
           <span className={letterClass}>H</span>
           <span className={`${letterClass} _12`}>i,</span>
           <br />
-          <span className={`${letterClass} _12`}>I</span>
-          <span className={`${letterClass} _12`}>'m</span>
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
           <img src={LogoTitle} alt="developer" />
-          
-          <AnimatedLetters letterClass={letterClass}
-          strArray={nameArray}
-          idx={15}/>
+
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
           <br />
-          <AnimatedLetters letterClass={letterClass}
-          strArray={jobArray}
-          idx={22}/>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
         </h1>
         <h2>Frontend Developer / JavaScript Expert / YouTuber</h2>
         <Link to="/contact" className="flat-button">
