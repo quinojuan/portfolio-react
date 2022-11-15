ddb53add919ec0c9e7ebfcacae508adb7e2fcf4f
Juan Quino <quinojuan@gmail.com>
2022-11-15 18:49:45 -0300

    constructing contact page

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  let funcionAuxiliar = () =>
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+  useEffect(() => {
+    funcionAuxiliar()
+  }, [])
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+            praesentium alias molestiae doloremque sequi provident at esse fugit
+            quo aliquid voluptatum eum, a odio sunt placeat repudiandae, minus
+            beatae fugiat.
+          </p>
+          <div className="contact-form">
+            <form action="">
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    name="message"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
