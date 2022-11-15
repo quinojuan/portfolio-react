53f09441de90da2addcbd81629432c977861f7d6
Juan Quino <quinojuan@gmail.com>
2022-11-15 00:03:49 -0300

    about finished

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,6 +1,8 @@
+import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
+import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -8,7 +10,7 @@ const About = () => {
   let funcionAuxiliar = () =>
     setTimeout(() => {
       setLetterClass('text-animate-hover')
-    }, 4000)
+    }, 3000)
 
   useEffect(() => {
     funcionAuxiliar()
@@ -38,6 +40,30 @@ const About = () => {
           laboriosam iure nulla alias nesciunt voluptatibus magni obcaecati
           omnis, iste eos explicabo in! Adipisci, suscipit?
         </p>
+      </div>
+
+      <div className='stage-cube-cont'>
+        <div className="cubespinner">
+            <div className="face1">
+                <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
+            </div>
+            <div className="face2">
+                <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
+            </div>
+            <div className="face3">
+                <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
+            </div>
+            <div className="face4">
+                <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
+            </div>
+            <div className="face5">
+                <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
+            </div>
+            <div className="face6">
+                <FontAwesomeIcon icon={faGitAlt} color="#EC4D28"/>
+            </div>
+        </div>
+
       </div>
     </div>
   )
