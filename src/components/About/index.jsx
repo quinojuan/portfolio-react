0d3ce7b75188160d57c11026a060e3df0055a043
Juan Quino <quinojuan@gmail.com>
2022-11-15 18:24:29 -0300

    about finished

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,7 +2,15 @@ import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
-import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faNodeJs,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -17,55 +25,58 @@ const About = () => {
   }, [])
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Necessitatibus, dignissimos officia labore officiis perferendis atque
-          laboriosam iure nulla alias nesciunt voluptatibus magni obcaecati
-          omnis, iste eos explicabo in! Adipisci, suscipit?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Necessitatibus, dignissimos officia labore officiis perferendis atque
-        </p>
-        <p>
-          laboriosam iure nulla alias nesciunt voluptatibus magni obcaecati
-          omnis, iste eos explicabo in! Adipisci, suscipit?
-        </p>
-      </div>
-
-      <div className='stage-cube-cont'>
-        <div className="cubespinner">
-            <div className="face1">
-                <FontAwesomeIcon icon={faAngular} color="#DD0031"/>
-            </div>
-            <div className="face2">
-                <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
-            </div>
-            <div className="face3">
-                <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
-            </div>
-            <div className="face4">
-                <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
-            </div>
-            <div className="face5">
-                <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
-            </div>
-            <div className="face6">
-                <FontAwesomeIcon icon={faGitAlt} color="#EC4D28"/>
-            </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Necessitatibus, dignissimos officia labore officiis perferendis
+            atque laboriosam iure nulla alias nesciunt voluptatibus magni
+            obcaecati omnis, iste eos explicabo in! Adipisci, suscipit?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Necessitatibus, dignissimos officia labore officiis perferendis
+            atque
+          </p>
+          <p>
+            laboriosam iure nulla alias nesciunt voluptatibus magni obcaecati
+            omnis, iste eos explicabo in! Adipisci, suscipit?
+          </p>
         </div>
 
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faNodeJs} color="#215732" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <Loader type='pacman' />
+    </>
   )
 }
 
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,6 +4,7 @@ import './index.scss'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from '../Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -35,35 +36,38 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="cantainer home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
+    <>
+      <div className="cantainer home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
 
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Frontend Developer / JavaScript Expert / YouTuber</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Frontend Developer / JavaScript Expert / YouTuber</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type='pacman' />
+    </>
   )
 }
 
